7346d90e918909bf94d7bb5d17f34bc864ae9e09
Sahu <dibyasha.sahu@capgemini.com>
2016-07-14 19:09:57 +0530

    employeeserviceupdated

--- Employee Management System/src/com/flp/ems/service/IEmployeeService.java
+++ Employee Management System/src/com/flp/ems/service/IEmployeeService.java
@@ -1,5 +1,11 @@
 package com.flp.ems.service;
 
 public interface IEmployeeService {
-
+	public  void  AddEmployee();
+	public  void  ModifyEmployee();
+	public  void  RemoveEmployee();
+	public  void  SerachEmployee();
+	public  void  getAllEmployee();
+	
 }
+
