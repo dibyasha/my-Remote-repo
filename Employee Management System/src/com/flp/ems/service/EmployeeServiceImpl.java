af54cfddb88683782d91a606072b92ff055f94bb
Sahu <dibyasha.sahu@capgemini.com>
2016-07-14 19:07:22 +0530

    employeeservice

--- Employee Management System/src/com/flp/ems/service/EmployeeServiceImpl.java
+++ Employee Management System/src/com/flp/ems/service/EmployeeServiceImpl.java
@@ -1,5 +1,45 @@
 package com.flp.ems.service;
+import java.util.Map;
+import java.util.Map.Entry;
 
-public class EmployeeServiceImpl {
+import com.flp.ems.dao.EmployeeDaoImplForList;
+import com.flp.ems.domain.Employee;
+
+public class EmployeeServiceImpl  implements IEmployeeService{
+	
+	Employee o2= new Employee();
+
+	@Override
+	public void AddEmployee(Map E) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ModifyEmployee() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void RemoveEmployee() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SerachEmployee() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getAllEmployee() {
+		// TODO Auto-generated method stub
+		
+	}()
+
+
+	
 
 }
